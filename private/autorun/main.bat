540a8a27ae4733bc7255cc80f013cb8f72b99aad
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-08-31 19:36:00 -0400

    Update main.bat

--- private/autorun/main.bat
+++ private/autorun/main.bat
@@ -2,4 +2,4 @@
 echo Bonjour Noémie, mes lunettes sont sales, je n'arrive pas à savoir si c'est vraiment toi pourrais tu faire ton code ?
 echo (Au cas où tu avais pas compris, je ne suis pas conne donc enweille le code tout de suite)
 set /p input= Password : 
-if %1%==Password (echo "Salut Noémie") else (echo "Mauvais mot de passe, bonne chance pour trouver les trucs de Noémie, fun à toi ! (Ne va pas private, pitier)")
+if %input%==Password (echo "Salut Noémie") else (echo "Mauvais mot de passe, bonne chance pour trouver les trucs de Noémie, fun à toi ! (Ne va pas private, pitier)")
